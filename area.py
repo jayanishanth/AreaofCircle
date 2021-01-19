262ebf5776c2505a15b73c3dd626ba95e562ff8f
jayanishanth <77501202+jayanishanth@users.noreply.github.com>
2021-01-19 21:49:04 +0530

    Created area.py
    This file prints area of circle based on given input of radius.

---
+++ area.py
@@ -0,0 +1,6 @@
+import math
+r = float(input(print("Input the radius of circle : "))
+def area_of_circle(r):
+ a=r*r*math.pi 
+ return a
+print("The area of the circle with radius %f is: %.2f"%r,%a)
